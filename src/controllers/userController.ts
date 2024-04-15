57a8ce03f0ec01390520586ef518ac95826df6e6
Gabriel DUSHIME <108078955+GabrielDushime@users.noreply.github.com>
2024-04-15 10:22:39 +0200

    users integration

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -22,11 +22,11 @@ export const userSignup = async (req: Request, res: Response) => {
     const user = new User({ firstName, lastName, email, password: hashedPassword });
     await user.save();
     res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    res.status(500).json({ message: 'Signup failed', error });
+  } catch (error:any) {
+    console.error('Signup failed:', error);
+    res.status(500).json({ message: 'Signup failed', error: error.message }); // Return error message
   }
-};
-
+};  
 export const userSignin = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
